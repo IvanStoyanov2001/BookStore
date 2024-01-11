4429c44604ef940ae01437150fafe9c5e25c35cb
Ivan Stoyanov <zonicbooom88@gmail.com>
2024-01-11 17:22:20 +0200

    a

---
+++ BookStore/BookStore.Models/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Models.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public int AuthorId { get; set; }
+    }
+}
