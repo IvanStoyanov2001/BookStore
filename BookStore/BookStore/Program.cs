d2ce493aec31f37105180c175336a761a464c31f
Ivan Stoyanov <zonicbooom88@gmail.com>
2024-01-18 17:34:37 +0200

    a

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -11,16 +11,24 @@ namespace BookStore
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services
+                     builder.Services
                 .AddSingleton<IBookRepository, BookRepository>();
             builder.Services
                 .AddSingleton<IBookService, BookService>();
-
+            builder.Services
+               .AddSingleton<IAuthorRepository, AuthorRepository>();
+            builder.Services
+                .AddSingleton<IAuthorService, AuthorService>();
+            builder.Services
+                .AddSingleton<ILibraryService, LibraryService>();
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services
+                .AddFluentValidationAutoValidation();
+            builder.Services
+                .AddValidatorsFromAssemblyContaining(typeof(Program));
 
 
 
---
+++ BookStore/BookStore/validator/GetAllBooksByAuthorRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Models.Requests;
+using FluentValidation;
+
+
+namespace BookStore.validator
+{
+    public class GetAllBooksByAuthorRequestValidator:AbstractValidator<GetAllBooksByAuthorRequest>
+    {
+        public GetAllBooksByAuthorRequestValidator() 
+        {
+            RuleFor(expression: X :GetAllBooksByAuthorRequest =>X.Time)
+                .NotNull()
+                .GreaterThan(valueToCompare: 0)
+                .LessThan(valueToCompare: 1000)
+                .WithMessage("Some error");
+
+            RuleFor(expression: X:GetAllBooksByAuthorRequest =>X.Name)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLenght(2)
+                .MaximumLenght(10);
+        }
+    }
+}
